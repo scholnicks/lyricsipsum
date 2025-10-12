dce37e62379684fa499df60add8990cd9b565032
Steve Scholnick <scholnicks@gmail.com>
2025-10-12 09:36:06 -0400

    added support for a configuration file

--- src/lyricsipsum/__main__.py
+++ src/lyricsipsum/__main__.py
@@ -20,10 +20,11 @@ Options:
 """
 
 import json
-import os
+# import os
 import random
 import re
 import sys
+import tomllib
 from dataclasses import asdict, dataclass
 from pathlib import Path
 
@@ -42,7 +43,10 @@ class Song:
 def main() -> None:
     """Main Method"""
     global arguments
-    arguments = docopt(__doc__, version="lyricsipsum 1.0.0")
+    arguments = docopt(__doc__, version="lyricsipsum 1.1.0")
+
+    if not configDirectory().exists():
+        configDirectory().mkdir(parents=True, exist_ok=True)
 
     if arguments["--save"]:
         saveLyricsToFile()
@@ -58,7 +62,7 @@ def main() -> None:
 def readLyricsFromFile() -> list[Song]:
     """Reads lyrics from a file and returns a list of Song objects"""
     try:
-        with open(jsonPath(), "r") as f:
+        with jsonPath().open("r") as f:
             return [Song(**song) for song in json.load(f)]
     except FileNotFoundError:
         print(f"No lyrics file found at {jsonPath()}. Please run with --save to create one.")
@@ -67,28 +71,43 @@ def readLyricsFromFile() -> list[Song]:
 
 def saveLyricsToFile() -> None:
     """Fetches lyrics from Genius and saves them to a file"""
-    genius: Genius = Genius(os.environ.get("GENIUS_ACCESS_TOKEN", ""))
-    genius.verbose = arguments["--verbose"]
-    genius.skip_non_songs = True
-    genius.excluded_terms = ["(Remix)", "(Live)"]
-    genius.remove_section_headers = True
-    genius.timeout = 15
-
-    songs: list[Song] = readLyricsFromFile() if Path(jsonPath()).exists() else []
-    artist = genius.search_artist(arguments["--save"], max_songs=int(arguments["--number"]), sort="popularity")
+    songs: list[Song] = readLyricsFromFile() if jsonPath().exists() else []
+    artist = buildGenius().search_artist(arguments["--save"], max_songs=int(arguments["--number"]), sort="popularity")
     for song in artist.songs:
         if song.lyrics:
             songs.append(Song(title=song.title, lyrics=re.sub(r"\n+", "\n", song.lyrics).strip()))
 
-    with open(Path.home() / ".lyricsipsum.json", "w") as f:
+    with jsonPath().open("w") as f:
         json.dump([asdict(s) for s in songs], f, indent=4)
 
     print(f"Saved {len(songs)} songs to {jsonPath()}")
 
 
-def jsonPath() -> str:
+def jsonPath() -> Path:
     """Returns the path to the JSON file containing lyrics"""
-    return str(Path.home() / ".lyricsipsum.json")
+    return configDirectory() / "songs.json"
+
+
+def buildGenius() -> Genius:
+    """Builds and returns a Genius object"""
+    config: dict = {}
+    configFile: Path = configDirectory() / "config.toml"
+    if configFile.exists():
+        with configFile.open("rb") as f:
+            config = tomllib.load(f)
+
+    genius: Genius = Genius(os.environ.get("GENIUS_ACCESS_TOKEN", config.get("client", {}).get("access_token", "")))
+    genius.verbose = arguments["--verbose"] or config.get("client", {}).get("verbose", False)
+    genius.skip_non_songs = config.get("client", {}).get("skip_non_songs", True)
+    genius.excluded_terms = config.get("client", {}).get("excluded_terms", ["(Remix)", "(Live)"])
+    genius.remove_section_headers = config.get("client", {}).get("remove_section_headers", True)
+    genius.timeout = int(config.get("client", {}).get("timeout", 15))
+    return genius
+
+
+def configDirectory() -> Path:
+    """Returns the path to the configuration directory"""
+    return Path.home() / ".config" / "lyricsipsum"
 
 
 if __name__ == "__main__":
