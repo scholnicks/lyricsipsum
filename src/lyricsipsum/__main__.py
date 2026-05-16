fb3b2d8c940c708d4fdd5b0686a5a6849713cda5
Steve Scholnick <scholnicks@gmail.com>
2026-05-16 15:01:24 -0400

    upgraded dependencies

--- src/lyricsipsum/__main__.py
+++ src/lyricsipsum/__main__.py
@@ -43,7 +43,7 @@ class Song:
 def main() -> None:
     """Main Method"""
     global arguments
-    arguments = docopt(__doc__, version="lyricsipsum 1.1.6")
+    arguments = docopt(__doc__, version="lyricsipsum 1.1.7")
 
     if not configDirectory().exists():
         configDirectory().mkdir(parents=True, exist_ok=True)
